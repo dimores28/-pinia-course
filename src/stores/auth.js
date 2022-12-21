57babab2ef8855b234681894ecc2f636c16079ca
Dmytro <dimores95@gmail.com>
2022-12-21 14:25:08 +0200

    async auth actions

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -2,20 +2,24 @@ import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore("auth", {
     state: () => ({
-        isAuthenticated: true,
-        user:{
-            name: "Sarthak",
-            email: "arthak@bitfumes.com"
-        }
+        isAuthenticated: false,
+        user:{}
     }),
+    getters: {
+        fullname: (state) => `${state.user.first_name} ${state.user.last_name}`,
+    },
     actions: {
         logout() {
             this.$patch((state) => {
                 (state.isAuthenticated = false), (state.user = {});
               });
         },
-        login() {
-            this.$reset();
+        async login() {
+          const res = await fetch("https://reqres.in/api/users/2");
+          const { data } = await res.json();
+
+          this.user = data;
+          this.isAuthenticated = true;
         }
     }
 });
