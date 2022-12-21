5784fcdde82a98b9e4920c750cf01baee97df136
Dmytro <dimores95@gmail.com>
2022-12-21 15:10:09 +0200

    store with composition API

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,25 +1,24 @@
 import { defineStore } from "pinia";
+import { computed, reactive, ref } from "vue";
 
-export const useAuthStore = defineStore("auth", {
-    state: () => ({
-        isAuthenticated: false,
-        user:{}
-    }),
-    getters: {
-        fullname: (state) => `${state.user.first_name} ${state.user.last_name}`,
-    },
-    actions: {
-        logout() {
-            this.$patch((state) => {
-                (state.isAuthenticated = false), (state.user = {});
-              });
-        },
-        async login() {
-          const res = await fetch("https://reqres.in/api/users/2");
-          const { data } = await res.json();
+export const useAuthStore = defineStore("auth", ()=> {
+    const isAuthenticated = ref(false);
+    const user = reactive({});
+    const fullname = computed(()=>  `${user?.value?.first_name} ${user?.value?.last_name}`)
 
-          this.user = data;
-          this.isAuthenticated = true;
-        }
+
+    function logout() {
+        this.$patch((state) => {
+            (state.isAuthenticated = false), (state.user = {});
+            });
     }
+    async function login() {
+        const res = await fetch("https://reqres.in/api/users/2?delay=2");
+        const { data } = await res.json();
+
+        this.user.value = data;
+        this.isAuthenticated = true;
+    }
+
+    return {logout, login, isAuthenticated, user, fullname};
 });
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,27 +1,29 @@
 import {defineStore} from "pinia"
+import { computed, ref } from "vue";
 import { useAuthStore } from "./auth"
 
-export const useCounterStore = defineStore("counter", {
-    state: () => ({
-        count: 10
-    }),
-    getters: {
-        countDigitLength: (state) => state.count.toString().length,
-    },
-    actions:{
-        increment() {
-            if(!this.isAuthenticated()) return;
+export const useCounterStore = defineStore("counter", () => {
+    const count = ref(0);
 
-            this.count++;
-        },
-        decrement() {
-            if(!this.isAuthenticated()) return;
-            
-            this.count--;
-        },
-        isAuthenticated() {
-            const authStore = useAuthStore();
-            return authStore.isAuthenticated;
-        }    
+    const countDigitLength = computed(()=> {
+        return count.value.toString().length;
+    });
+
+
+    function increment() {
+        if(!isAuthenticated()) return;
+
+        this.count++;
     }
+    function decrement() {
+        if(!isAuthenticated()) return;
+
+        this.count--;
+    }
+    function isAuthenticated() {
+        const authStore = useAuthStore();
+        return authStore.isAuthenticated;
+    }    
+
+    return {increment, decrement, count, countDigitLength};
 })  
